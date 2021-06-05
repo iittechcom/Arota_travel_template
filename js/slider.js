519ec0c119ae5f9a57cd972019f23d059cf4e7bf
DinhHoa1997 <bunsroi123@gmail.com>
2021-06-05 11:16:39 +0700

    hcmit

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,7 @@ $('.owl-banner').owlCarousel({
     loop: true,
     nav: true,
     dots: false,
-    navText: ["<i class='fal fa-arrow-circle-left'></i>", "<i class='fal fa-arrow-circle-right'></i>"],
+    navText: ["<i class='fal fa-chevron-left'></i>", "<i class='fal fa-chevron-right'></i>"],
     responsive: {
         0: {
             items: 1,
@@ -12,6 +12,29 @@ $('.owl-banner').owlCarousel({
         },
         1000: {
             items: 1,
+        }
+    }
+})
+$('.owl-destination').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    navText: ["<i class='fal fa-chevron-left'></i>", "<i class='fal fa-chevron-right'></i>"],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+            margin: -5,
+            stagePadding: 190,
         }
     }
 })
