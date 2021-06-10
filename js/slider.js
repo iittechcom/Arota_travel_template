dfea2413bf1ea71ef2d3526dbd2212c3629a8be8
DinhHoa1997 <bunsroi123@gmail.com>
2021-06-10 08:29:38 +0700

    cmit

--- js/slider.js
+++ js/slider.js
@@ -38,3 +38,27 @@ $('.owl-destination').owlCarousel({
         }
     }
 })
+$('.owl-experience').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    navText: ["<i class='fal fa-chevron-left'></i>", "<i class='fal fa-chevron-right'></i>"],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 2,
+            margin: 15,
+            stagePadding: 120,
+            center: true,
+        }
+    }
+})
