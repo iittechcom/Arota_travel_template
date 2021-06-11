fa4a9a07314fac8ce78610e7d55307308f53a642
DG94-NVT <tonnguyencn94@gmail.com>
2021-06-11 12:10:33 +0700

    update 3 page Incentive

--- js/page.js
+++ js/page.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+
+    /* ---- Destination - pages ---- */
     $('.tour-slider').owlCarousel({
         loop: true,
         center: true,
@@ -8,7 +11,7 @@ $(document).ready(function() {
         responsiveClass: true,
         responsive: {
             0: {
-                items: 2
+                items: 1
             },
             600: {
                 items: 3
@@ -18,6 +21,41 @@ $(document).ready(function() {
             }
         }
 
+    })
+
+    $(".nav-next").click(function() {
+        $(this).parents(".tour-slider").find(".owl-next").click();
+    });
+
+    $(".nav-prev").click(function() {
+        $(this).parents(".tour-slider").find(".owl-prev").click();
+    });
+
+    /* Incentives - pages */
+    $('.indes__avatar').owlCarousel({
+        items: 1,
+        loop: false,
+        center: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        URLhashListener: true,
+        startPosition: 'URLHash'
+    });
+
+    $('.indes__thumb').owlCarousel({
+        items: 3,
+        loop: false,
+        mouseDrag: false,
+        touchDrag: false,
+        margin: 10,
+        nav: false,
+        dots: false
+    })
+
+    $('.indes__thumb .item').click(function() {
+        $(this).parents(".indes__thumb").find(".item").removeClass("b-white");
+        $(this).addClass("b-white");
     })
 
 })
